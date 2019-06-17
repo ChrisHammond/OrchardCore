332ad4cc9d3f7c1624766e56b2fc6fd4282dfe4b
Chris Hammond <christoc@live.com>
2019-06-16 23:08:29 -0500

    Checking in CollectorOfJackTheme

---
+++ src/OrchardCore.Themes/CollectorOfJackTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Collector Of Jack Theme",
+    Author = "Christoc.com Software Solutions",
+    Website = "https://www.christoc.com",
+    Version = "2.0.0",
+    Description = "A theme adapted for the CollectorOfJack blog."
+)]
